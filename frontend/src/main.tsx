4f608bc33ac11cb7b4cc1f5b018a8ced6cf6a3bd
redwheeler3 <jeffo@jeffo.net>
2026-08-22 20:05:22 -0700

    refactor(frontend): split feature stylesheets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
 import "./styles.css";
+import "./styles/ranking-print.css";
+import "./styles/quality-feedback.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
-
